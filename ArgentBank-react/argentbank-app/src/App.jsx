16ebe94beb9077fbbb129420e9b6dd2503e8f264
Bruce Doby <dobybruce@gmail.com>
2025-04-22 15:53:20 +0200

    Léger changement + optimisation des images en format webp et réduction de leur taille

--- ArgentBank-react/argentbank-app/src/App.jsx
+++ ArgentBank-react/argentbank-app/src/App.jsx
@@ -3,21 +3,13 @@ import Chat from './assets/img/icon-chat.webp'
 import Money from './assets/img/icon-money.webp'
 import Security from './assets/img/icon-security.webp'
 import FeatureItem from './components/feature-item'
-import BackgroundImage from './assets/img/bank-tree.webp'
+import Hero from './components/hero'
 
 function App() {
 
   return (
     <>
-    <div className="hero" style={{backgroundImage: `url(${BackgroundImage})`}}>
-      <section className="hero-content">
-        <h2 className="sr-only">Promoted Content</h2>
-        <p className="subtitle">No fees.</p>
-        <p className="subtitle">No minimum deposit.</p>
-        <p className="subtitle">High interest rates.</p>
-        <p className="text">Open a savings account with Argent Bank today!</p>
-      </section>
-    </div>
+    <Hero />
     <section className='features'>
       <FeatureItem image={Chat} text='You are our #1 priority' subtext='Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.' />
       <FeatureItem image={Money} text='More savings means higher rates' subtext='The more you save with us, the higher your interest rate will be!' />
